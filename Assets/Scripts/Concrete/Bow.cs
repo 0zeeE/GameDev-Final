0ee69ca00e34c76c109eeaabecc60277c494f533
0zeeE <ertekinozgur2000@hotmail.com>
2024-12-24 22:05:16 +0300

    Yay Kontrol scripti yazildi
    Envanter sistemi hazir oldugunda kullanilacak.

--- Assets/Scripts/Concrete/Bow.cs
+++ Assets/Scripts/Concrete/Bow.cs
@@ -10,6 +10,7 @@ public class Bow : MonoBehaviour
     public GameObject arrow;
     private Animator bowAnimator;
     public GameObject[] arrowVisual;
+    public bool isBowActive = true;
 
     void Start()
     {
@@ -19,13 +20,20 @@ public class Bow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        if(Input.GetMouseButton(0) && isBowActive)
         {
             bowAnimator.SetBool("Aim",true);
         }
         else
         {
             bowAnimator.SetBool("Aim", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            isBowActive = !isBowActive;
+            bowAnimator.enabled = !bowAnimator.enabled;
+            bowAnimator.Play("Idle");
         }
     }
 
@@ -46,11 +54,15 @@ public class Bow : MonoBehaviour
 
     public void ShootArrow()
     {
-        GameObject arrow_prefab = Instantiate(arrow, arrowSpawnPoint);
-        arrow_prefab.transform.SetParent(null);
-        arrow_prefab.GetComponent<Rigidbody>().AddForce(arrowSpawnPoint.forward * arrowSpeed, ForceMode.Impulse);
+        if(isBowActive)
+        {
+            GameObject arrow_prefab = Instantiate(arrow, arrowSpawnPoint);
+            arrow_prefab.transform.SetParent(null);
+            arrow_prefab.GetComponent<Rigidbody>().AddForce(arrowSpawnPoint.forward * arrowSpeed, ForceMode.Impulse);
 
-        Destroy(arrow_prefab, 10);
+            Destroy(arrow_prefab, 10);
+        }
+        
     }
 
     public void DisplayArrow()
