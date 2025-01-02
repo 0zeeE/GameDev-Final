3277056e102ef3e831a4e7b1774e5e96d731d311
Zeynep Kübra Batmaz <z.k.batmaz@gmail.com>
2025-01-03 00:07:34 +0300

    yer altı 8 kat, cyclops ve skeletonlar eklendi

---
+++ Assets/Scripts/SkeletonWithSword.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonWithSword : Enemy
+{
+    public override void Update()
+    {
+        base.CheckPlayerState();
+
+        if (!playerInSightRange && !playerInAttackRange)
+        {
+            Patrol(); // Devriye fonksiyonu
+        }
+        else if (playerInSightRange && !playerInAttackRange)
+        {
+            ChasePlayer(); // Oyuncuyu kovala
+        }
+        else if (playerInSightRange && playerInAttackRange)
+        {
+            AttackPlayer(); // Oyuncuya saldýr
+        }
+    }
+
+
+    public override void Patrol()
+    {
+        base.Patrol();
+        enemyAnim.SetBool("isWalking", true);
+        Debug.Log("Patrol animasyonu tetiklendi mi? " + enemyAnim.GetBool("isWalking"));
+    }
+
+    public override void ChasePlayer()
+    {
+        base.ChasePlayer();
+        enemyAnim.SetBool("isWalking", false);
+        enemyAnim.SetBool("isRunning", true);
+        Debug.Log("Chase animasyonu tetiklendi mi? " + enemyAnim.GetBool("isRunning"));
+    }
+
+    public override void AttackPlayer()
+    {
+        Debug.Log("Attack baþladý!");
+        agent.SetDestination(transform.position);
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            int attackType = Random.Range(0, 2); // 0 = Attack1, 1 = Attack2, 2 = PowerAttack
+
+            switch (attackType)
+            {
+                case 0:
+                    enemyAnim.SetTrigger("Attack1");
+                    break;
+                case 1:
+                    enemyAnim.SetTrigger("Attack2");
+                    break;
+            }
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    public override void TakeDamage(float amount)
+    {
+        base.TakeDamage(amount);
+        if (!isDead)
+        {
+            enemyAnim.SetTrigger("Hit");
+        }
+    }
+
+    public override void Die()
+    {
+        if (!isDead)
+        {
+            isDead = true;
+            enemyAnim.SetTrigger("Death");
+            agent.enabled = false;
+            Destroy(gameObject, 5f); // Ölümden sonra 5 saniye sonra yok et
+        }
+    }
+}
