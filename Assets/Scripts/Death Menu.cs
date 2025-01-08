1a39e7495bb28fe5bc6ff154913e828324624deb
Zeynep Kübra Batmaz <z.k.batmaz@gmail.com>
2025-01-08 15:11:27 +0300

    ölüm ekranı ve cutscene

---
+++ Assets/Scripts/Death Menu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+    
+    Application.Quit();
+#endif
+    }
+}
